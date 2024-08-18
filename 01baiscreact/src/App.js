9607fdd76cb872e7f353da361373face3386693b
jagtapsakshi <jagtapsakshi12@gmail.com>
2024-08-18 10:15:59 +0530

    Created app using npx and npm

---
+++ 01baiscreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1> React JS course | This is my first React application code </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+
+  return (
+    <h1>Chai or React Course</h1>
+  )
+}
+
+export default App
+
